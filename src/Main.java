69fbeeb41cf07b544d928c11c07b6f4813221f27
yamurzuko <yamurzcan@gmail.com>
2022-12-04 10:35:17 +0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+/*
+        String str = "Hello worldlslks!";
+        System.out.println(str);
+
+        int a;
+        Scanner input = new Scanner(System.in);
+        System.out.print("a sayisini giriniz :");
+        a = input.nextInt();
+        System.out.println(a);
+
+ */
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lütfen Matemetik Notunu Giriniz:");
+        mat = input.nextInt();
+        System.out.print("Lütfen Fizik Notunu Giriniz:");
+        fizik = input.nextInt();
+        System.out.print("Lütfen Kimya Notunu Giriniz:");
+        kimya = input.nextInt();
+        System.out.print("Lütfen Turkce Notunu Giriniz:");
+        turkce = input.nextInt();
+        System.out.print("Lütfen Tarih Notunu Giriniz:");
+        tarih = input.nextInt();
+        System.out.print("Lütfen Muzik Notunu Giriniz:");
+        muzik = input.nextInt();
+
+        Double result = (mat + fizik + kimya + turkce + tarih + muzik) / 6.0;
+
+        System.out.print("Ortalama :" + result);
+    }
+}
